6c2492ddc3c9dbecd1c290751ad4892fad07ab16
magistrdev <magistrdev@gmail.com>
2020-04-19 04:00:11 +0300

    up

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <fcntl.h>
+//#include <iostream>
 
 #include "SDL2/SDL.h"
 
@@ -6,6 +11,13 @@ const int WIDTH = 800, HEIGHT = 600;
 
 int main( int argc, char *argv[] )
 {
+    int fd;
+    char bla[501];
+    fd = open("hop.txt", O_RDWR);
+    int fr = read(fd, &bla, 500);
+    printf("%s \n", bla);
+    write(fd, "hophophop\n",10);
+    close(fd);
     SDL_Init( SDL_INIT_EVERYTHING );
 
     SDL_Window *window = SDL_CreateWindow( "Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI );
@@ -14,13 +26,13 @@ int main( int argc, char *argv[] )
     if ( NULL == window )
     {
         // In the case that the window could not be made...
-        std::cout << "Could not create window: " << SDL_GetError( ) << std::endl;
+        //std::cout << "Could not create window: " << SDL_GetError( ) << std::endl;
         return 1;
     }
     
     SDL_Event windowEvent;
     
-    while ( true )
+    while (1)
     {
         if ( SDL_PollEvent( &windowEvent ) )
         {
